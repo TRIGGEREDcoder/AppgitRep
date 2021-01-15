69b367e88cd563fc80a8f2084236cc7a094cd6e3
Appswift <ciap.in@capgemini.com>
2021-01-15 16:46:34 +0300

    Initial commit

---
+++ SQL/SQLQuery.sql
@@ -0,0 +1,2 @@
+CREATE TABLE "gitapp1" ("f1" CHAR (10), "f2" INT, "f3" CHAR (30), "f4" CHAR (30) NOT NULL, CONSTRAINT "pk_constraint_gitapp1" PRIMARY KEY ("f1", "f2"));
+CREATE TABLE "gitapp2" ("f12" CHAR (10), "f22" INT, "f32" CHAR (30), "f42" CHAR (30) NOT NULL, CONSTRAINT "pk_constraint_gitapp2" PRIMARY KEY ("f12", "f22"));
